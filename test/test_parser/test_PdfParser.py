374a74ad3882d3d928efb9bf323bd5d670a12afd
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-09 20:36:48 -0500

    added test

---
+++ test/test_parser/test_PdfParser.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+
+from src.parser.PdfParser import PdfParser
+
+
+class TestPdfParser(unittest.TestCase):
+    PATH_TO_TEST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "files", "parser"))
+
+    def test_getWordFrequency_pdfFile_happyPath(self):
+        docxFilePath = os.path.join(self.PATH_TO_TEST_DIR, "test.pdf")
+        wf = PdfParser.getWordFrequency(docxFilePath)
+        self.assertIsInstance(wf, dict)
+        self.assertEqual(6, len(wf.keys()))
+        self.assertEqual(2, wf["hello"])
+        self.assertEqual(2, wf["world"])
+        self.assertEqual(1, wf["this"])
+        self.assertEqual(1, wf["is"])
+        self.assertEqual(1, wf["a"])
+        self.assertEqual(1, wf["test"])
