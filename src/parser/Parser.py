07b010c1125468b0c42bc7a1b2f7365e0be27528
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-09 20:21:12 -0500

    added test and fixed parser

--- src/parser/Parser.py
+++ src/parser/Parser.py
@@ -17,8 +17,10 @@ class Parser(ABC):
             words = line.split()
             for word in words:
                 word = word.translate(str.maketrans('', '', string.punctuation)).lower()
-                if word in wordFrequency:
-                    wordFrequency[word] += 1
-                else:
-                    wordFrequency[word] = 1
+                # do not add empty strings
+                if word != "":
+                    if word in wordFrequency:
+                        wordFrequency[word] += 1
+                    else:
+                        wordFrequency[word] = 1
         return wordFrequency
--- test/test_parser/test_DocParser.py
+++ test/test_parser/test_DocParser.py
@@ -1,7 +1,20 @@
+import os
 import unittest
+
+from src.parser.DocParser import DocParser
 
 
 class TestDocParser(unittest.TestCase):
+    PATH_TO_TEST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "files", "parser"))
 
-    def test_getWordFrequency_happyPath(self):
-        ...
+    def test_getWordFrequency_docxFile_happyPath(self):
+        docxFilePath = os.path.join(self.PATH_TO_TEST_DIR, "test.docx")
+        wf = DocParser.getWordFrequency(docxFilePath)
+        self.assertIsInstance(wf, dict)
+        self.assertEqual(6, len(wf.keys()))
+        self.assertEqual(2, wf["hello"])
+        self.assertEqual(2, wf["world"])
+        self.assertEqual(1, wf["this"])
+        self.assertEqual(1, wf["is"])
+        self.assertEqual(1, wf["a"])
+        self.assertEqual(1, wf["test"])
