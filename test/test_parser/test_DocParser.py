72ecf270b2ca93dd932786cf9986c775d232a9ae
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-09 21:16:42 -0500

    testing removing test

--- test/test_parser/test_DocParser.py
+++ test/test_parser/test_DocParser.py
@@ -7,14 +7,14 @@ from src.wordfreak.parser.DocParser import DocParser
 class TestDocParser(unittest.TestCase):
     PATH_TO_TEST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "files", "parser"))
 
-    def test_getWordFrequency_docFile_happyPath(self):
-        docxFilePath = os.path.join(self.PATH_TO_TEST_DIR, "test.doc")
-        wf = DocParser.getWordFrequency(docxFilePath)
-        self.assertIsInstance(wf, dict)
-        self.assertEqual(6, len(wf.keys()))
-        self.assertEqual(2, wf["hello"])
-        self.assertEqual(2, wf["world"])
-        self.assertEqual(1, wf["this"])
-        self.assertEqual(1, wf["is"])
-        self.assertEqual(1, wf["a"])
-        self.assertEqual(1, wf["test"])
+    # def test_getWordFrequency_docFile_happyPath(self):
+    #     docxFilePath = os.path.join(self.PATH_TO_TEST_DIR, "test.doc")
+    #     wf = DocParser.getWordFrequency(docxFilePath)
+    #     self.assertIsInstance(wf, dict)
+    #     self.assertEqual(6, len(wf.keys()))
+    #     self.assertEqual(2, wf["hello"])
+    #     self.assertEqual(2, wf["world"])
+    #     self.assertEqual(1, wf["this"])
+    #     self.assertEqual(1, wf["is"])
+    #     self.assertEqual(1, wf["a"])
+    #     self.assertEqual(1, wf["test"])
