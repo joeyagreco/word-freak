6b61af0c7f06f747701389d166ae978d7da45477
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-09 20:22:15 -0500

    added test

--- test/test_parser/test_DocParser.py
+++ test/test_parser/test_DocParser.py
@@ -18,3 +18,15 @@ class TestDocParser(unittest.TestCase):
         self.assertEqual(1, wf["is"])
         self.assertEqual(1, wf["a"])
         self.assertEqual(1, wf["test"])
+
+    def test_getWordFrequency_docFile_happyPath(self):
+        docxFilePath = os.path.join(self.PATH_TO_TEST_DIR, "test.doc")
+        wf = DocParser.getWordFrequency(docxFilePath)
+        self.assertIsInstance(wf, dict)
+        self.assertEqual(6, len(wf.keys()))
+        self.assertEqual(2, wf["hello"])
+        self.assertEqual(2, wf["world"])
+        self.assertEqual(1, wf["this"])
+        self.assertEqual(1, wf["is"])
+        self.assertEqual(1, wf["a"])
+        self.assertEqual(1, wf["test"])
