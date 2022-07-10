785fdbd0ef154d2b96a8f98b04d290b1f6f4709c
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-09 23:09:08 -0500

    updated setup and bumped version

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+from pathlib import Path
+
 import setuptools
+
+thisDirectory = Path(__file__).parent
+longDescription = (thisDirectory / "README.md").read_text()
 
 with open("LICENSE") as f:
     license = f.read()
 
 setuptools.setup(
     name="wordfreak",
-    version="0.0.8",
+    version="0.0.9",
     author="Joey Greco",
     author_email="joeyagreco@gmail.com",
     description="Word Freak is a Python library that extracts word frequencies from files.",
+    long_description=longDescription,
     license=license,
     packages=setuptools.find_packages(exclude=("test", "docs")),
     install_requires=["PyPDF2",
