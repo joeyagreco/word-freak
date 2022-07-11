a83cc34ae0bce92f10757b218e0633550babf9e5
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-11 17:24:45 -0500

    removed test file

--- test.py
+++
@@ -1,5 +0,0 @@
-from wordfreak import extractWordFrequencies
-
-if __name__ == "__main__":
-    extractWordFrequencies("C:\\Users\\14143\\PycharmProjects\\wordfreak\\test\\files\\parser\\test.txt",
-                           "C:\\Users\\14143\\PycharmProjects\\wordfreak\\test\\files\\parser\\test.json")
