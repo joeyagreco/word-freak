684f691131bdaf14f8a7924b45efa911d8090609
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-13 22:41:35 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE") as f:
 
 setuptools.setup(
     name="wordfreak",
-    version="0.0.12",
+    version="1.0.0",
     author="Joey Greco",
     author_email="joeyagreco@gmail.com",
     description="Word Freak is a Python library that extracts word frequencies from files.",
--- wordfreak/__init__.py
+++ wordfreak/__init__.py
@@ -1,3 +1,3 @@
 from .service.wordFrequency import extractWordFrequencies, pythonizeWordFrequencies
 
-__version__ = '0.0.12'
+__version__ = "1.0.0"
